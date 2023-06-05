1495d97cd2d2383b886b8c646ca9e33c3247ffbd
Jan Mrak <janmrak1@gmail.com>
2023-06-05 18:31:37 +0200

    message command parser checks

--- src/router.ts
+++ src/router.ts
@@ -4,12 +4,14 @@ import { logger } from "./logger/pino";
 import { CommandsEnum } from "./enums/commands-enum";
 import { ping } from "./commands";
 
-const prefix_length:number = 1;
+const prefix = "!"
 export const router = (message: Message ):void => {
-    let args:string[] = message.content.slice(prefix_length).trim().split(/ +/g);
-    let command:string = args[0].toLowerCase();
 
-    logger.debug(command)
+    if (message.author.bot || !message.content.startsWith(prefix)) return;
+
+    let args:string[] = message.content.slice(prefix.length).trim().split(/ +/g);
+    let command:string = args[0].toLowerCase();
+    logger.info(message.content)
     switch (command){
         case CommandsEnum.Ping: ping(message);
     }
