d8d6be233134b07fea333b109f4b50abfb384d01
Jan Mrak <janmrak1@gmail.com>
2023-06-07 23:03:05 +0200

    db connection class added

---
+++ src/db/DB.ts
@@ -0,0 +1,29 @@
+require("dotenv").config();
+import mongoose, { Connection, Mongoose } from 'mongoose';
+import {logger} from "../logger/pino";
+export class MongoDB {
+    private connection: Connection | null;
+
+    constructor() {
+        this.connection = null;
+    }
+
+    public async connect(): Promise<void> {
+        try {
+            this.connection = await mongoose.createConnection(`mongodb://${process.env.DB_URL!}/${process.env.DB_NAME!}`, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            } as mongoose.ConnectOptions);
+            logger.info('Connected to MongoDB');
+        } catch (error) {
+            logger.error('Error connecting to MongoDB:', error);
+        }
+    }
+
+    public disconnect(): void {
+        if (this.connection) {
+            this.connection.close();
+            console.log('Disconnected from MongoDB');
+        }
+    }
+}
