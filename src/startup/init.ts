898727ea9410294a4e9bf67f1eddeec2edfe76c2
Jan Mrak <janmrak1@gmail.com>
2023-06-13 23:38:27 +0200

    removed init.ts

--- src/startup/init.ts
+++
@@ -1,6 +0,0 @@
-import {Client} from "discord.js";
-import {MongoDB} from "../db/index";
-export const __init__ = (client: Client): void => {
-    let db:MongoDB = new MongoDB();
-    db.connect();
-}
