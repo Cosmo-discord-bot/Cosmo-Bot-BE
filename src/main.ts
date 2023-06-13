6718c7be2c4b849ae649e9b26c2196b67be49c97
Jan Mrak <janmrak1@gmail.com>
2023-06-13 23:40:15 +0200

    config is saved when bot is added to server

--- src/main.ts
+++ src/main.ts
@@ -29,4 +29,21 @@ client.once(Events.ShardReady, async () => {
 client.on(Events.MessageCreate, message => router(message));
 client.on(Events.InteractionCreate, async interaction => HandleSlashCommands(interaction));
 
+client.on(Events.GuildCreate, async guild => {
+    const mainChannel: GuildBasedChannel | undefined = guild.channels.cache.find(
+        channel => channel.name === 'general'
+    );
+
+    if (!mainChannel) return;
+
+    const conf: ConfigModel = {
+        guildId: guild.id,
+        prefix: '!',
+        color: '#00FF00',
+        mainChannel: mainChannel.id,
+    };
+    client.config?.insertNewConfig(conf);
+    client.config?.loadConfig();
+});
+
 client.login(process.env.DISCORD_TOKEN);
