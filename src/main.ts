77338a0b3f5b1233f5580818af5a92af9d1947b5
Jan Mrak <janmrak1@gmail.com>
2023-06-13 23:39:05 +0200

    using custom client for connecting

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,32 @@
-import {__init__} from "./startup/init";
+import { Config } from './startup/Config/Config';
 
-require("dotenv").config();
-import { logger } from "./logger/pino";
-import { Partials, GatewayIntentBits, Events } from 'discord.js';
-import { router } from "./router";
-import { HandleSlashCommands } from "./slash-commands/set-slash-commands";
-import { CustomClient } from "./Classes/CustomClient";
+require('dotenv').config();
+import { logger } from './logger/pino';
+import { Partials, GatewayIntentBits, Events, GuildBasedChannel } from 'discord.js';
+import { router } from './router';
+import { HandleSlashCommands } from './slash-commands/set-slash-commands';
+import { CustomClient } from './Classes/CustomClient';
+import { ConfigModel } from './interfaces/ConfigModel';
 
-
-const client = new CustomClient({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildVoiceStates],
-    partials: [Partials.GuildScheduledEvent, Partials.GuildMember]
+const client: CustomClient = new CustomClient({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+    partials: [Partials.GuildScheduledEvent, Partials.GuildMember],
 });
 
-client.once("ready", () => {
-    logger.info("Ready!");
-    __init__(client);
+client.once(Events.ShardReady, async () => {
+    logger.info('Ready!');
+    await client.__initClient__().then(() => {
+        logger.info(client.config?.configs.get('123'));
+    });
 });
-client.on("messageCreate", (message) => router(message));
+client.on(Events.MessageCreate, message => router(message));
 client.on(Events.InteractionCreate, async interaction => HandleSlashCommands(interaction));
 
 client.login(process.env.DISCORD_TOKEN);
