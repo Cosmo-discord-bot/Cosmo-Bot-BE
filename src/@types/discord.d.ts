e1c7950cf66210bc4ae58ab77f98f80bc32f7843
Jan Mrak <janmrak1@gmail.com>
2023-10-19 23:13:16 +0200

    fix: config in discord types class

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -1,7 +1,9 @@
-import { Collection } from "discord.js";
+import { Collection } from 'discord.js';
+import { Config } from '../dbInteractions/Config';
 
-declare module "discord.js" {
+declare module 'discord.js' {
     export interface Client {
         commands: Collection<String, any>;
+        config: Config;
     }
 }
