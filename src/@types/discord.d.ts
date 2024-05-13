87c6ce167584bba4a7a61412b928d777fccc72aa
Jan Mrak <janmrak1@gmail.com>
2024-05-13 23:39:36 +0200

    fix: import discord.d.ts

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -1,5 +1,6 @@
 import { Collection } from 'discord.js';
 import { EventsDB } from '../dbInteractions/EventsDB';
+import { ConfigDB } from '../dbInteractions/ConfigDB';
 
 declare module 'discord.js' {
     export interface Client {
