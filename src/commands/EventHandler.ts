1c629d91c3c09f349c5592868f60c57d57bfedb5
Jan Mrak <janmrak1@gmail.com>
2024-05-19 22:28:43 +0200

    feat: Events manager completed

--- src/commands/EventHandler.ts
+++ src/commands/EventHandler.ts
@@ -1,17 +1,23 @@
 import {
     CategoryChannel,
+    ChannelType,
     Client,
     Guild,
+    GuildBasedChannel,
     GuildScheduledEvent,
     GuildScheduledEventStatus,
+    MessageReaction,
     PartialGuildScheduledEvent,
+    PermissionFlagsBits,
+    ReactionCollector,
     Role,
     User,
 } from 'discord.js';
 import { logger } from '../logger/pino';
 import { sanitizeEventName } from '../helper/sanitizeNames';
 import { IEvent } from '../interfaces/IEvent';
-
+// TODO - Channels from ended events should be moved to Previous Events category
+// TODO - Check if channels should be threads
 export class EventHandler {
     private guild: Guild;
     private client: Client;
@@ -58,10 +64,14 @@ export class EventHandler {
                 color: 'White',
                 reason: 'Event role for ' + sanitizedEventName,
             });
-
-            // TODO - Role to event creator
-
+            if (!event.creatorId) {
+                throw new Error('Event creator not found');
+            }
+            this.guild.members.cache.get(event.creatorId)?.roles.add(createdRole);
             logger.info(`Role created: ${createdRole.name} - ${createdRole.id}`);
+
+            const mentionedRoles: string[] = this.parseEventDescriptionForMentions(event);
+            this.sendEventLinkToGeneral(event, mentionedRoles);
 
             const eventData: IEvent = {
                 eventId: event.id,
@@ -72,35 +82,7 @@ export class EventHandler {
                 channelId: '-1',
             };
             logger.debug(`Event data: ${JSON.stringify(eventData)}`);
-            let mentionedRoles: string[] = [];
-            if (event.description) {
-                //const description = event.description;
-                const mentions = event.description.match(/@[^ ]+/g);
-                logger.debug(`Mentions: ${mentions}`);
-                if (mentions) {
-                    mentions.forEach(mention => {
-                        // Check for mentioned roles
-                        const role = this.guild.roles.cache.find(
-                            role => role.name.toLowerCase() === mention.substring(1).toLowerCase()
-                        );
-                        if (role) {
-                            mentionedRoles.push(role.id);
-                        }
-                    });
-                    logger.debug(`Mentioned roles: ${mentionedRoles}`);
-                }
-                logger.debug(`Event description: ${event.description}`);
-            }
 
-            let mainChannel = this.guild.channels.cache.get(
-                this.client.config.configs.get(this.guild.id)!.mainChannelId
-            );
-            if (mainChannel && mainChannel.isTextBased()) {
-                const mentionedRolesStr = createTaggableRoleStringFromIds(mentionedRoles);
-                // TODO - Reactions for channel creation
-                mainChannel.send(`${event.url} - ${mentionedRolesStr}`);
-                logger.debug(`Event URL: ${event.url} - ${mentionedRolesStr}`);
-            }
             this.client.events.insertEvent(eventData);
         } catch (error) {
             logger.error(`Error creating role for event: ${error}`);
@@ -197,12 +179,6 @@ export class EventHandler {
             logger.info(`Role deleted: ${roleId}`);
 
             this.client.events.deleteEvent(event.id);
-            /*
-            if (foundEvent.channelId != '-1') {
-                this.guild.channels.cache.find(channel => channel.id === foundEvent.channelId)?.delete();
-                logger.info(`Channel deleted: ${foundEvent.channelId}`);
-            }
-            */
         } catch (error) {
             logger.error(`Error deleting event: ${error}`);
         }
@@ -240,6 +216,75 @@ export class EventHandler {
         }
     }
 
+    private parseEventDescriptionForMentions(event: GuildScheduledEvent | PartialGuildScheduledEvent): string[] {
+        let mentionedRoles: string[] = [];
+        if (event.description) {
+            const mentions = event.description.match(/@[^ ]+/g);
+            logger.debug(`Mentions: ${mentions}`);
+            if (mentions) {
+                mentions.forEach(mention => {
+                    // Check for mentioned roles
+                    const role = this.guild.roles.cache.find(
+                        role => role.name.toLowerCase() === mention.substring(1).toLowerCase()
+                    );
+                    if (role) {
+                        mentionedRoles.push(role.id);
+                    }
+                });
+                logger.debug(`Mentioned roles: ${mentionedRoles}`);
+            }
+            logger.debug(`Event description: ${event.description}`);
+        }
+        return mentionedRoles;
+    }
+
+    private async sendEventLinkToGeneral(
+        event: GuildScheduledEvent | PartialGuildScheduledEvent,
+        mentionedRoles: string[]
+    ) {
+        let mainChannel = this.guild.channels.cache.get(this.client.config.configs.get(this.guild.id)!.mainChannelId);
+        if (mainChannel && mainChannel.isTextBased()) {
+            const mentionedRolesStr = createTaggableRoleStringFromIds(mentionedRoles);
+            let message = await mainChannel.send(`${event.url} - ${mentionedRolesStr}`);
+            logger.debug(`Event URL: ${event.url} - ${mentionedRolesStr}`);
+            await message.react('✅');
+            await message.react('❌');
+
+            const filter = (reaction: MessageReaction, user: User) => {
+                if (reaction && reaction.emoji && reaction.emoji.name && user) {
+                    return ['✅', '❌'].includes(reaction.emoji.name);
+                }
+                return false;
+            };
+
+            const collector: ReactionCollector = message.createReactionCollector({ filter, time: 60_000 });
+
+            collector.on('collect', (reaction, user) => {
+                if (reaction.emoji.name === '✅' && user.id === event.creatorId) {
+                    if (!this.isChannelCategoryCreated()) {
+                        this.createChannelCategory();
+                    }
+                    this.createEventChannel(event);
+                    collector.stop('Event creator created channel');
+                    logger.debug(`Creating event channel for ${event.id} ${event.name}`);
+                }
+                if (reaction.emoji.name === '❌' && user.id === event.creatorId) {
+                    collector.stop('Event creator cancelled channel creation');
+                    logger.debug(`Event creator cancelled channel creation`);
+                }
+            });
+
+            collector.on('end', collected => {
+                message.reactions.removeAll().catch(error => {
+                    logger.error(`Error removing reactions from ${event.id} ${event.name}: ${error}`);
+                });
+                logger.info(`Collected ${collected.size} reactions for ${event.id} ${event.name}`);
+            });
+        } else {
+            logger.error('sendEventLinkToGeneral - Main channel not found');
+        }
+    }
+
     private getRoleFromEvent(event: GuildScheduledEvent | PartialGuildScheduledEvent): Role {
         const foundEvent = this.client.events.events.get(event.id);
         if (!foundEvent) {
@@ -256,7 +301,64 @@ export class EventHandler {
         return role;
     }
 
-    private promptTextChannelCreation() {}
+    private isChannelCategoryCreated(): boolean {
+        if (this.eventsCategory) return true;
+        else {
+            const channel: GuildBasedChannel | undefined = this.guild.channels.cache.get('events');
+            if (channel) {
+                if (channel.type === ChannelType.GuildCategory) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private async createChannelCategory() {
+        this.eventsCategory = await this.guild.channels.create({
+            name: 'events',
+            type: ChannelType.GuildCategory,
+            position: 0.1,
+            reason: 'Events category where future events will have their channels created',
+        });
+    }
+
+    private async createEventChannel(event: GuildScheduledEvent | PartialGuildScheduledEvent) {
+        if (!this.eventsCategory) {
+            throw new Error('Events category not found');
+        }
+        if (!event.name) {
+            throw new Error('Event name not found');
+        }
+        const sanitizedEventName: string = sanitizeEventName(event.name);
+        const eventChannel = await this.guild.channels.create({
+            name: sanitizedEventName,
+            type: ChannelType.GuildText,
+            parent: this.eventsCategory,
+            reason: 'Event channel for ' + sanitizedEventName,
+            permissionOverwrites: [
+                {
+                    id: this.guild.roles.everyone.id,
+                    deny: [PermissionFlagsBits.ViewChannel],
+                },
+                {
+                    id: this.getRoleFromEvent(event).id,
+                    allow: [PermissionFlagsBits.ViewChannel],
+                },
+            ],
+        });
+        logger.info(`Event channel created: ${eventChannel.name} - ${eventChannel.id}`);
+
+        this.client.events.updateEvents({
+            eventId: event.id,
+            guildId: event.guildId,
+            eventsCategoryId: this.eventsCategory.id,
+            eventName: event.name,
+            roleId: this.getRoleFromEvent(event).id,
+            channelId: eventChannel.id,
+        });
+    }
+
     /*
     Maybe this should be a different handler
     private static subscribeToEvent() {}
