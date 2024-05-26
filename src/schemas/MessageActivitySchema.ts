c61098878a82644106950886438d3797896da306
Jan Mrak <janmrak1@gmail.com>
2024-05-26 12:41:34 +0200

    feat: prep for message activity statistics

---
+++ src/schemas/MessageActivitySchema.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { IGuildMessageActivity, IMessageActivity } from '../interfaces/IMessageActivity';
+
+const messageActivitySchema: mongoose.Schema<IMessageActivity> = new mongoose.Schema<IMessageActivity>(
+    {
+        ts: { type: Number, required: true },
+        channelId: { type: String, required: true },
+        userId: { type: String, required: true },
+    },
+    { _id: false }
+);
+
+export const guildMessageActivitySchema: mongoose.Schema<IGuildMessageActivity> =
+    new mongoose.Schema<IGuildMessageActivity>({
+        guildId: { type: String, required: true, unique: true },
+        activities: [messageActivitySchema],
+    });
