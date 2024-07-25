d3023dbbd44e66054243ac3799de38429a7e9aa3
Jan Mrak <janmrak1@gmail.com>
2024-07-25 23:59:16 +0200

    refactor: moved service events in own folder
    Signed-off-by: Jan Mrak <janmrak1@gmail.com>

---
+++ src/events/expressEvents.ts
@@ -0,0 +1,44 @@
+import http from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
+import cors from 'cors';
+import { rtr } from '../api/router';
+import { CustomClient } from '../definitions/Classes/CustomClient';
+import { logger } from '../logger/pino';
+
+export const expressEvents = (client: CustomClient) => {
+    /*
+    Express api server initialization
+    */
+
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        cors: {
+            origin: process.env.CORS_ORIGIN || '*',
+            methods: ['GET', 'PUT', 'POST', 'DELETE'],
+        },
+    });
+    const port: string | 3000 = process.env.EXPRESS_PORT || 3000;
+
+    app.use(
+        cors({
+            origin: process.env.CORS_ORIGIN || '*', // Specify the allowed origin
+            methods: 'GET,PUT,POST,DELETE', // Specify allowed HTTP methods
+        })
+    );
+    app.use(express.json());
+    app.use('/api/v1', rtr(client, io));
+
+    io.on('connection', (socket) => {
+        console.log('A client connected');
+
+        socket.on('disconnect', () => {
+            console.log('A client disconnected');
+        });
+    });
+
+    server.listen(port, () => {
+        logger.info(`API is running at http://localhost:${port}`);
+    });
+};
